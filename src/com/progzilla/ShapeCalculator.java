0d97685c686174d4bd461ef5bd50c3178e5088f9
Ola Popoola <oladipupo.popoola@gmail.com>
2026-08-18 12:27:09 +0100

    added derived class Rectangle

--- src/com/progzilla/ShapeCalculator.java
+++ src/com/progzilla/ShapeCalculator.java
@@ -2,6 +2,7 @@ package com.progzilla;
 
 public class ShapeCalculator {
 
+    // Parent Class
     static class Shape{
         private String name;
         protected String color;
@@ -52,6 +53,60 @@ public class ShapeCalculator {
 
         public void displayData(String prefix){
             System.out.printf("%s - Shape: %s, Color: %s%n", prefix, name, color);
+        }
+
+    }
+
+    // Derived class
+    static class Rectangle extends Shape {
+
+        private double width;
+        private double height;
+
+        public Rectangle(String name, String color, double width, double height){
+            super(name, color);
+            this.width = width;
+            this.height = height;
+            this.type = "Rectangle";
+        }
+
+        public double getWidth(){
+            return width;
+        }
+
+        public void setWidth(double width){
+            if (width > 0) {
+                this.width = width;
+            }else {
+                System.out.println("Width value must be positive");
+            }
+        }
+
+        public double getHeight(){
+            return height;
+        }
+
+        public void setHeight(double height){
+            if(height > 0){
+                this.height = height;
+            }else{
+                System.out.println("Height value has to be positive");
+            }
+        }
+
+        @Override
+        public double calculateArea(){
+            return width * height;
+        }
+
+        @Override
+        public void displayData(){
+            super.displayData();
+            System.out.printf(" Dimensions: %s.2f x %.2f, Area: %.2f%n", width, height, calculateArea());
+        }
+
+        public double calculateArea(double scale){
+            return calculateArea() * scale;
         }
 
     }
