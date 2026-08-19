70bd1c86a6df1b7b0dc6b6c04bab9183fdb38f0c
Ola Popoola <oladipupo.popoola@gmail.com>
2026-08-19 01:47:07 +0100

    implemented circle subsclass

--- src/com/progzilla/ShapeInterface.java
+++ src/com/progzilla/ShapeInterface.java
@@ -1,4 +1,26 @@
 package com.progzilla;
 
-public class ShapeInterface {
+interface ShapeInterface {
+    double area();
+    double perimeter();
+}
+
+class Circle implements ShapeInterface {
+
+    private final double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double area(){
+        return Mat.PI * radius * radius;
+    }
+
+    @Override
+    public double perimeter(){
+        return 2 * Math.PI * radius;
+    }
+
 }
