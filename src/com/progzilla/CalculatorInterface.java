1ba50f1d389e71b5988ab03715535ec36a4b37b6
Ola Popoola <oladipupo.popoola@gmail.com>
2026-08-19 02:13:51 +0100

    rename class and add main class

--- src/com/progzilla/CalculatorInterface.java
+++ src/com/progzilla/CalculatorInterface.java
@@ -1,7 +1,5 @@
 package com.progzilla;
 
-import org.w3c.dom.css.Rect;
-
 interface ShapeInterface {
     double area();
     double perimeter();
@@ -44,5 +42,26 @@ class Rectangle implements ShapeInterface{
     @Override
     public double perimeter() {
         return 2 * (width * height);
+    }
+}
+
+public class CalculatorInterface {
+    public static void main(String[] args){
+        ShapeInterface circle = new Circle(5.0);
+        ShapeInterface rectangle = new Rectangle(3.2, 5.0);
+
+        System.out.println("Circle (r-5): area=" + circle.area() + ", perimeter=" + circle.perimeter());
+        System.out.println("Rectangle (4x6): area=" + rectangle.area() + ", perimeter="+ rectangle.perimeter());
+
+        ShapeInterface[] shapeInterfaces = {
+                new Circle(3),
+                new Rectangle(2,4),
+                new Circle(9)
+        };
+        double totalArea = 0;
+        for (ShapeInterface s : shapeInterfaces){
+            totalArea += s.area();
+        }
+        System.out.println("\nTotal of all shapes: "+ totalArea);
     }
 }
